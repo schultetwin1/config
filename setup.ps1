5155b59c51e7249d378c481e75fd429bf77f6df1
Matt Schulte <maschu@microsoft.com>
2019-02-22 09:34:38 +0100

    Fixed up powershell setup

--- setup.ps1
+++ setup.ps1
@@ -8,7 +8,7 @@ if (!(Test-Path $HOME\vimfiles\autoload)) {
 if (!(Test-Path $HOME\vimfiles\bundle)) {
     md $HOME\vimfiles\bundle
 }
-wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -OutFile $HOME\vimfiles\autoload\pathogen.vim
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim" -OutFile $HOME\vimfiles\autoload\pathogen.vim
 if (!(Test-Path $HOME\vimfiles\bundle\vim-ps1)) {
     git clone https://github.com/PProvost/vim-ps1.git $HOME\vimfiles\bundle\vim-ps1
 }
@@ -16,7 +16,11 @@ if (!(Test-Path $HOME\vimfiles\bundle\vim-ps1)) {
 if (!(Test-Path $HOME\vimfiles\bundle\typescript-vim)) {
     git clone https://github.com/leafgarland/typescript-vim.git $HOME\vimfiles\bundle\typescript-vim
 }
-copy $PSScriptRoot\vimrc $HOME\_vimrc
-copy $PSScriptRoot\Microsoft.PowerShell_profile.ps1 $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+Copy-Item -Path $PSScriptRoot\vimrc -Destination $HOME\_vimrc
+
+if (-not (Test-Path -Path $PROFILE)) {
+    New-Item -Path $PROFILE -ItemType File -Force | Out-Null
+}
+Copy-Item -Path $PSScriptRoot\Microsoft.PowerShell_profile.ps1 -Destination $PROFILE
 . $PROFILE
 Write-Host "Done"
